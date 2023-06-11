5f3423f23203cd3ef4723cea9645ce51f5ad2eec
Alexander <Lisnevskiy14@gmail.com>
2023-06-11 23:08:49 +0300

    Added docstrings

--- src/classes/file_data_managers/abstract_file_data_manager.py
+++ src/classes/file_data_managers/abstract_file_data_manager.py
@@ -2,14 +2,26 @@ from abc import ABC, abstractmethod
 
 
 class FileDataManager(ABC):
+    """
+    Абстрактный класс для работы с данными о вакансиях в файле.
+    """
     @abstractmethod
     def add_vacancy(self, vacancy):
+        """
+        Абстрактный метод для добавления вакансии в файл.
+        """
         pass
 
     @abstractmethod
     def get_vacancy_by_id(self, vid):
+        """
+        Абстрактный метод для получения вакансии из файла по идентификатору.
+        """
         pass
 
     @abstractmethod
     def delete_vacancy(self, vid):
+        """
+        Абстрактный метод для удаления вакансии из файла по идентификатору.
+        """
         pass
